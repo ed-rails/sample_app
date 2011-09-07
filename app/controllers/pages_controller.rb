924132003b579d471e357b9cd322d98f9de02eff
unknown <vittorina.g@tiscali.it>
2011-09-07 21:46:40 +0200

    mod routes, home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,13 @@ class PagesController < ApplicationController
   def home
   end
 
-<<<<<<< HEAD
   def contact
   end
 
-  def about 
-=======
+  def about
+  end 
+
   def controller
   end
 
-  def about
->>>>>>> 0a347d654f484463f890cb6414f3ee0de01d0be7
-  end
 end
