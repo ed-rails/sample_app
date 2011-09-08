b556e1e7d22ab310c414fe3eb907aba814fe939e
edo <vittorina.g@tiscali.it>
2011-09-08 20:01:23 +0200

    start css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,16 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
+  end
+
+  def contact
+    @title = "Contact"
   end
 
   def controller
   end
 
   def about
+    @title = "About"
   end
 end
