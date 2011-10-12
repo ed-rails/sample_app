fa5dfe9e661908c093910772080785f2da011a13
unknown <vittorina.g@tiscali.it>
2011-10-12 17:46:15 +0200

    put

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -101,4 +101,30 @@ describe UsersController do
     response.should have_selector("title",  :content => "Sign up")
   end
 
+  describe "GET 'edit'" do 
+
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+
+    it "should be succesful" do
+      get :edit, :id => @user
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get :edit, :id => @user
+      response.should have_selector("title", :content => "Edit user")
+    end
+ 
+    it "should have a link to change the Gravatar" do
+      get :edit, :id => @user
+      gravater_url = "http://gravatar.com/emails"
+      response.should have_selector("a", :href => gravatar_url,
+                                         :content => "change")
+    end
+
+  end  
+
 end
